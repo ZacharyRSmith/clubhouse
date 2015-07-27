572d8b8547a72988378cc1ad2c1c895714d7a40c
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-07-27 15:38:54 +0000

    Finish adding User sign up ability

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid signup info" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { username: "",
+                               email: "user@invalid",
+                               password: "pass",
+                               password_confirmation: "word" }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup info" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { username: "ExampleUser",
+                                            email:    "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+  end
 end
